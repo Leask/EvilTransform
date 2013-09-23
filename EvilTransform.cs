411a3ea0158bdabdc731359e8bd2fc190f0ce2c7
Leask Huang <i@leaskh.com>
2013-09-23 16:26:17 +0800

    adding EvilTransform.cs

---
+++ EvilTransform.cs
@@ -0,0 +1,72 @@
+//
+// Copyright (C) 1000 - 9999 Somebody Anonymous
+// NO WARRANTY OR GUARANTEE
+//
+
+using System;
+
+namespace Navi
+{
+    class EvilTransform
+    {
+        const double pi = 3.14159265358979324;
+
+        //
+        // Krasovsky 1940
+        //
+        // a = 6378245.0, 1/f = 298.3
+        // b = a * (1 - f)
+        // ee = (a^2 - b^2) / a^2;
+        const double a = 6378245.0;
+        const double ee = 0.00669342162296594323;
+
+        //
+        // World Geodetic System ==> Mars Geodetic System
+        public static void transform(double wgLat, double wgLon, out double mgLat, out double mgLon)
+        {
+            if (outOfChina(wgLat, wgLon))
+            {
+                mgLat = wgLat;
+                mgLon = wgLon;
+                return;
+            }
+            double dLat = transformLat(wgLon - 105.0, wgLat - 35.0);
+            double dLon = transformLon(wgLon - 105.0, wgLat - 35.0);
+            double radLat = wgLat / 180.0 * pi;
+            double magic = Math.Sin(radLat);
+            magic = 1 - ee * magic * magic;
+            double sqrtMagic = Math.Sqrt(magic);
+            dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+            dLon = (dLon * 180.0) / (a / sqrtMagic * Math.Cos(radLat) * pi);
+            mgLat = wgLat + dLat;
+            mgLon = wgLon + dLon;
+        }
+
+        static bool outOfChina(double lat, double lon)
+        {
+            if (lon < 72.004 || lon > 137.8347)
+                return true;
+            if (lat < 0.8293 || lat > 55.8271)
+                return true;
+            return false;
+        }
+
+        static double transformLat(double x, double y)
+        {
+            double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.Sqrt(Math.Abs(x));
+            ret += (20.0 * Math.Sin(6.0 * x * pi) + 20.0 * Math.Sin(2.0 * x * pi)) * 2.0 / 3.0;
+            ret += (20.0 * Math.Sin(y * pi) + 40.0 * Math.Sin(y / 3.0 * pi)) * 2.0 / 3.0;
+            ret += (160.0 * Math.Sin(y / 12.0 * pi) + 320 * Math.Sin(y * pi / 30.0)) * 2.0 / 3.0;
+            return ret;
+        }
+
+        static double transformLon(double x, double y)
+        {
+            double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.Sqrt(Math.Abs(x));
+            ret += (20.0 * Math.Sin(6.0 * x * pi) + 20.0 * Math.Sin(2.0 * x * pi)) * 2.0 / 3.0;
+            ret += (20.0 * Math.Sin(x * pi) + 40.0 * Math.Sin(x / 3.0 * pi)) * 2.0 / 3.0;
+            ret += (150.0 * Math.Sin(x / 12.0 * pi) + 300.0 * Math.Sin(x / 30.0 * pi)) * 2.0 / 3.0;
+            return ret;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 EvilTransform
 =============
 
-WGS-84 to GCJ-02 
+WGS-84 to GCJ-02
+
+Original from
+https://on4wp7.codeplex.com/SourceControl/changeset/view/21483#353936
+
+See also:
+http://blog.csdn.net/coolypf/article/details/8686588
